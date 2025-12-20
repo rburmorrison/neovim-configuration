2f69e5b8d346c6dc2da813f6eefcb3b3f488ee2a
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2025-12-20 10:56:40 -0500

    :wrench: Fix and extend LSP-related keybindings

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -16,8 +16,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = ctx.buf, }), { bufnr = ctx.buf, }) end,
       { buffer = ctx.buf, desc = "Toggle inlay hints", })
 
-    vim.keymap.set("n", "<leader>la", function() MiniExtra.pickers.lsp({ scope = "references", }) end,
-      { buffer = ctx.buf, desc = "Code actions picker", })
+    vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { buffer = ctx.buf, desc = "Code actions picker", })
 
     vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = ctx.buf, desc = "Format buffer", })
 
@@ -26,9 +25,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>lR", function() MiniExtra.pickers.lsp({ scope = "references", }) end,
       { buffer = ctx.buf, desc = "References picker", })
 
-    vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { buffer = ctx.buf, desc = "Go to implementation", })
+    vim.keymap.set("n", "<leader>li", function() MiniExtra.pickers.lsp({ scope = "implementation", }) end,
+      { buffer = ctx.buf, desc = "Implementation picker", })
 
-    vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, { buffer = ctx.buf, desc = "Go to type definition", })
+    vim.keymap.set("n", "<leader>lt", function() MiniExtra.pickers.lsp({ scope = "type_definition", }) end,
+      { buffer = ctx.buf, desc = "Type definition picker", })
 
     vim.keymap.set("n", "<leader>lO", function() MiniExtra.pickers.lsp({ scope = "document_symbol", }) end,
       { buffer = ctx.buf, desc = "Document symbol picker", })
