39497b0a60bb1236b234192792d9e921fdb106ab
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2025-12-20 10:57:52 -0500

    :wrench: Add a keybinding for `MiniNotify` to show notifications

--- lua/plugins/mini-notify.lua
+++ lua/plugins/mini-notify.lua
@@ -3,5 +3,8 @@ return {
     "nvim-mini/mini.notify",
     version = "*",
     opts = {},
+    keys = {
+      { "<leader>n", function() MiniNotify.show_history() end, desc = "Notification History", },
+    },
   },
 }
