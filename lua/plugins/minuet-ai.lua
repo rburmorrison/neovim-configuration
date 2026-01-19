aac0165eab07d4c17c1f5f47d99d2065ec4d9ccf
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2026-01-19 12:05:13 -0500

    :wrench: Use Mercury Coder for Minuet

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -4,7 +4,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", },
     version = "*",
     opts = {
-      provider = "codestral",
+      provider = "openai_fim_compatible",
+      provider_options = {
+        openai_fim_compatible = {
+          model = "mercury-coder",
+          end_point = "https://api.inceptionlabs.ai/v1/fim/completions",
+          api_key = "INCEPTION_API_KEY",
+          stream = true,
+        },
+      },
       virtualtext = {
         auto_trigger_ft = { "*", },
         auto_trigger_ignore_ft = {
