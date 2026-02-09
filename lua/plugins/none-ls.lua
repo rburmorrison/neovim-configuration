95968dd3bb43a5331fb28d7d55487a3e6a1c7fbb
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2026-02-09 18:57:41 -0500

    :wrench: Remove prettier from none-ls configuration

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,9 +9,7 @@ return {
       local null_ls = require("null-ls")
 
       null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.prettier,
-        },
+        sources = {},
       })
     end,
   },
